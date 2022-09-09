a617ca6129aee0c9c2c8dcd292b1d2dda863ec3b
Thulisile Ngoman <ThuleeN@outlook.com>
2022-09-09 10:49:31 -0700

    forward slash correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include<stdio.h>
-/*
- * * main - print string in the put function
- *
- * *
- *
- * * function
- *
- * * prints "Programming is like building a multilingual puzzle"
- *
- * * Return: Always 0 (Successful)
- *
- * /
 
- main()
-
+/**
+    * main - Printing
+    * *
+    * * Return: Always 0 (Successful)
+    * */
+int main(void)
 {
-
-		puts("Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
