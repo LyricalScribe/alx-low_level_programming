dafd9d66adcf3da985677ce6f3f73a8ce7d083b5
Thulisile Ngoman <ThuleeN@outlook.com>
2022-09-22 08:04:36 -0700

    Indent corecctions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 
 /**
  * _isupper - check if the code is uppercase
- *
  * Return: Return 1 if 'c' is uppercase otherwise always 0 (Success)
  */
 
 int _isupper(int c)
 
-{	if(c>= 65 && c<= 90)
+{
+	else if (c > = 65 && c < = 90)
 	return (1);
 	return (0);
 }
