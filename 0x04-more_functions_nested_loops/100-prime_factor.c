01671e8c4e0ae3289a71d9475e7b7d9d0f14f401
Thulisile Ngoman <ThuleeN@outlook.com>
2022-09-22 05:52:53 -0700

    Prime factor + Print triangle

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,13 +13,16 @@ int main(void)
 	long int n;
 	long int max;
 	long int i;
+
 	n = 612852475143;
 	max = -1;
+
 	while (n % 2 == 0)
 	{
 	max = 2;
 	n /= 2;
 	}
+
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 	while (n % i == 0)
