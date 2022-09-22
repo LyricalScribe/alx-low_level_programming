aa0adee0378c0f012ea818155146ead1281024f8
Thulisile Ngoman <ThuleeN@outlook.com>
2022-09-22 09:55:50 -0700

    isalpha fixes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * Return: Always 0.
+ * _isalpha - checks for alphabets
+ * @c: character to compare
+ * Return: Returns 0 or 1 depending on condition
  */
 
-int main(void)
-
+int _isalpha(int c);
 {
-	 int r;
-
-	 r = _isalpha('H');
-	 _putchar(r + '0');
-	 r = _isalpha('o');
-	 _putchar(r + '0');
-	 r = _isalpha(108);
-	 _putchar(r + '0');
-	 r = _isalpha(';');
-	 _putchar(r + '0');
-	 _putchar('\n');
+	return if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	{
+		return (1);
+	}
 	return (0);
 }
