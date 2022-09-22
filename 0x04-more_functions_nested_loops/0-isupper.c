816066ea833bdd31348df7e9d0055e6e29a80f1c
Thulisile Ngoman <ThuleeN@outlook.com>
2022-09-22 03:02:12 -0700

    0-isupper.c 1-2-3

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check if the code is uppercase
+ *
+ * Return: Return 1 if 'c' is uppercase otherwise always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+	if(c>=65 &&c<=90)
+	return (1);
+	return (0);
+}
