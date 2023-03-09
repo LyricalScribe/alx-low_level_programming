2d1f73162bcda99b010557a47dc82726db14efdf
Thulisil Ngomane <ThuleeN@outlook.com>
2023-03-09 23:27:52 +0200

    Script edits

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,18 @@
 
 /**
  * _puts_recursion - prints a string using recursion
- * @s: pointer to the string
+ * @s: string being printed
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1)
 	}
-	_putchar(*s)
-	_puts_recursion(s + 1);
+	else
+	{
+	_putchar('\n');
+	}
 }
