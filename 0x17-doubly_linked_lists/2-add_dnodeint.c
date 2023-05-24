1f2e4c4bc3af6b36851aa27801ff424e7c21121d
Thulisile <ThuleeN@outlook.com>
2023-05-25 00:55:39 +0200

    Fixed style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,33 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a list
- * @h: head of the list
- * Return: address of the new node or NULL on failure
+ * add_dnodeint - Adds a new node at the beginning of a list
+ * @head: Double pointer to the head of the list
+ * @n: Value to be assigned to the new node
+ *
+ * Return: Address of the new node or NULL on failure
  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
 
- dlistint_t *add_dnodeint(dlistint_t **head, const int n)
- {
- 	dlistint_t *new;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
- 	new = malloc(sizeof(dlistint_t));
- 	if (new == NULL)
- 		return (NULL);
+	new_node->prev = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
- 	new->prev = NULL;
- 	new->n = n;
- 	new->next = NULL;
- 	if (*head == NULL)
- 	{
- 		*head = new;
- 		return (new);
- 	}
- 	new->next = *head;
-	(*head)->prev = new;
-	*head = new;
-	return (new);
- }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	new_node->next = *head;
+	(*head)->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
