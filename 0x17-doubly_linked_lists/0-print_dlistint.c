f66a495e1e961b5ff95090623ce44c0fd62c2d12
Thulisile <ThuleeN@outlook.com>
2023-05-24 23:34:41 +0200

    fixed betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,14 +14,14 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *current = h;
 
 	if (h == NULL)
-	{
 		return (0);
-	}
+
 	while (current)
 	{
 		printf("%d\n", current->n);
 		n++;
 		current = current->next;
 	}
+
 	return (n);
 }
