63dc69fa559d8c590669e8de7bfd5afe565324cb
Thulisile <ThuleeN@outlook.com>
2023-06-11 21:03:11 +0200

    Fixed Betty style

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,35 +2,32 @@
 #include <stdlib.h>
 #include "main.h"
 
-/**		
- * _strdup - duplicate to new memory space location		
- * @str: char
- * Return: 0	
+/**
+ * _strdup - duplicate a string to a new memory space location
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string, NULL if failure
  */
-		
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *dup;
+	int i, len = 0;
 
 	if (str == NULL)
-
-		return (NULL);	
-	i = 0;
-
-	while (str[i] != '\0')
-
-		i++;
-
-		aaa = malloc(sizeof(char) * (i + 1));
-
-	if (aaa == NULL)
-
 		return (NULL);
 
-	for (r = 0; str[r]; r++)
+	while (str[len] != '\0')
+		len++;
 
-		aaa[r] = str[r];
+	dup = malloc(sizeof(char) * (len + 1));
 
-	return (aaa);
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0';
+
+	return (dup);
 }
