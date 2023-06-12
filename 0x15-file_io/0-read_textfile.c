7e146f54dadabba2a28b9c4f632d0b2f345a9de1
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-06-12 16:28:29 +0200

    betty on 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,34 +8,31 @@
  *
  * Return: Actual number of letters it could read and print
  */
- 
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t fd;
-	size_t wr = 0, sz;
+ssize_t read_textfile(const char * filename, size_t letters) {
+  ssize_t fd;
+  size_t wr = 0, sz;
 
-	char *c = malloc(sizeof(char) * letters);
+  char * c = malloc(sizeof(char) * letters);
 
-	if (!filename){
-		return (0);
-	}
+  if (!filename) {
+    return (0);
+  }
 
-	fd = open(filename, O_RDONLY);
+  fd = open(filename, O_RDONLY);
 
-	if (fd < 0){
-		return (0);
-	}
+  if (fd < 0) {
+    return (0);
+  }
 
-	sz = read(fd, c, letters);
+  sz = read(fd, c, letters);
 
-	c[sz] = '\0';
+  c[sz] = '\0';
 
-	wr = write(STDOUT_FILENO, c, sz);
+  wr = write(STDOUT_FILENO, c, sz);
 
-	free(c);
-	close(fd);
+  free(c);
+  close(fd);
 
-
-	return ((wr != sz) ? 0 : wr);
+  return ((wr != sz) ? 0 : wr);
 }
