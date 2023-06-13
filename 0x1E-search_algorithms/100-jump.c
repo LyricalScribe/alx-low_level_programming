5e565e821899240eeca68978117f3ab05aeaec47
Thulisile <ThuleeN@outlook.com>
2023-06-14 01:04:19 +0200

    Ran scripts

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,14 +2,14 @@
 
 /**
   * jump_search - Searches for a value in a sorted array
-  *               of integers using jump search
+  *   of integers using jump search
   * @array: A pointer to the first element of the array to search
   * @size: The number of elements in the array
   * @value: The value to search for
-  * Description: Prints a value every time it is compared in the array.
-  *              Uses the square root of the array size as the jump step.
+  * Description: Prints a value every time it is compared in the array
+  *   Uses the square root of the array size as the jump step
   * Return: If the value is not present or the array is NULL, -1
-  *         Otherwise, the first index where the value is located
+  *   Otherwise, the first index where the value is located
   */
 int jump_search(int *array, size_t size, int value)
 {
