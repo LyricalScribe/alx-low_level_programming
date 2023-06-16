8b54f4cc62cc95013bec7a972fa718c45b8606b0
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-06-16 21:54:28 +0200

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
+"""returns parameter of ofan island."""
 def island_perimeter(grid):
     """
     Calculates area of a grid
     """
-    if type(grid) is not list:
-            return
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-        i = 1
-
-        per = 0
-        wjile 1< len(grid) -1:
-                j = 1
-                while j < len(grid[i])-1:
-                    if grid[i][j] == 1:
-                            side = 4
-                            if grid[i-1] == 1:
-                                side -= 1
-                            if grid[i][j+1] == 1:
-                                    side -= 1
-                            per += side
-                    j += 1
-                return(per)
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j-1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i-1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
