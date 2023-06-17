331d3ba49c4fa8bb309c9f1a9bb16b39310916e5
Thulisile <ThuleeN@outlook.com>
2023-06-18 00:26:52 +0200

    typedef function fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,19 @@
  */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
+	char *name;
+	float age;
+	char *owner;
+};
 
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(const dog_t *d);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
-
